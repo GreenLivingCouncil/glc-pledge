f08ad344d7c6abeda7228f9e1e1a0ab509993310
sckoo <stephen.theking@gmail.com>
2013-05-29 01:47:43 -0700

    removed local angular libraries and old test "service"

--- js/services.js
+++ js/services.js
@@ -7,16 +7,5 @@
 // In this case it is a simple value service.
 angular.module('PledgeApp.services', [], function($provide) {
     $provide.value('version', '1.0');
-    /*
-    $provide.factory('config', ['$http', function(http) { 
-        http.get('config/events.json').success(function(eventsData) {
-            http.get('config/paths.json').success(function(pathsData) {
-                var result = { "events": eventsData, "paths": pathsData };
-                alert("hello");
-                return result;
-            });
-        });
-    }]);
-    */
 });
 
