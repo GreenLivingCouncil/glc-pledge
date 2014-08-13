5de8fa62fcb96679b9b2d2e51306fe662b5fd7f9
sckoo <stephen.theking@gmail.com>
2014-08-12 23:11:28 -0700

    update config sample

--- js/config.sample.js
+++ js/config.sample.js
@@ -1,85 +1,275 @@
-_admins = ["sckoo"];
-
 _paths = {
     "root": "http://www.stanford.edu/group/glc/cgi-bin/pledge/",
     "cgiBin": "/group/glc/cgi-bin/pledge/bin/"
 };
 
 _events = {
-    "waterwars": {
-        "link": "http://www.facebook.com/StanfordWaterWars",
-        "name": "Water Wars 2013",
-        "id": "0",
-        "pledgeText": "I pledge, to the best of my ability, to save water for the duration of the 2013 Water Wars competition, with the help of these guidelines:",
+    "ew2014week1": {
+        "link": "http://www.facebook.com/StanfordEnergyWars",
+        "name": "Week 1: Help keep the future bright, turn off the light!",
+        "active": true,
+        "id": "5",
+        "pledgeText": "This week...",
         "pledgeItems": [
-            "Be more efficient with my laundry.",
-            "Take shorter and more efficient showers.",
-            "Turn off the tap whenever I can."
+            "I will turn off the lights when I’m not in the room.",
+            "I will purchase energy efficient lighting.",
+            "I will replace incandescent light bulbs with more efficient light bulbs (fluorescent light bulbs (CFL) or light-emitting diode (LED) bulbs.",
+            "I will open the blinds to increase natural light in my room.",
+            "I will reduce background light levels and rely more on task lighting.",
+            "I will put a reminder near the light switch to remind myself to turn off the light (ask your Dorm Coordinator for one!).",
+            "I will turn off lights when I’m the last one leaving public rooms (computer clusters, bathrooms, laundry room, lounge)."
         ],
         "teamList": [
             {
-                "name": "EastFlo",
-                "size": "200",
+                "name": "Burbank",
+                "size": "94",
                 "id": "0"
             },
             {
-                "size": "267",
-                "name": "WestFlo",
+                "name": "Junipero",
+                "size": "93",
                 "id": "1"
             },
             {
-                "name": "Crothers Hall",
-                "size": "152",
+                "name": "Larkin",
+                "size": "104",
                 "id": "2"
             },
             {
-                "name": "Crothers Memorial",
-                "size": "228",
+                "name": "Twain",
+                "size": "106",
                 "id": "3"
             },
             {
-                "name": "Muwekma-Tah-Ruk",
-                "size": "31",
+                "name": "Donner",
+                "size": "94",
                 "id": "4"
             },
             {
-                "name": "Kimball",
-                "size": "209",
+                "name": "Serra",
+                "size": "92",
                 "id": "5"
             },
             {
-                "name": "Branner",
-                "size": "126",
+                "name": "Cedro",
+                "size": "93",
                 "id": "6"
+            },
+            {
+                "name": "Casa Zapata",
+                "size": "79",
+                "id": "7"
+            },
+            {
+                "name": "Rinconada",
+                "size": "93",
+                "id": "8"
+            },
+            {
+                "name": "Trancos",
+                "size": "82",
+                "id": "9"
+            },
+            {
+                "name": "Soto",
+                "size": "93",
+                "id": "10"
+            },
+            {
+                "name": "Okada",
+                "size": "76",
+                "id": "11"
+            },
+            {
+                "name": "Arroyo",
+                "size": "84",
+                "id": "12"
+            },
+            {
+                "name": "Otero",
+                "size": "91",
+                "id": "13"
             }
         ],
-        "auxMessage": "The team that has the greatest percentage of pledges on April 26th will win up to $500 in prizes! You can also create a link below to send to your friends in the competition and rack up referrals, possibly entailing cool prizes. :)"
+        "auxMessage": "It sounds almost too simple, but you can actually save a lot of energy by flipping the switch when you leave the room! THIS WEEK'S RAFFLE IS OVER. The winners of the five $20 Amazon gift certificates are: Kate Nelson from Burbank, Jesus Guzman and Colin Kimzey from Soto, Nick Burakoff from Trancos, and an anonymous pledger from Cedro!",
+        "logoSrc": "img/energywars2014.png"
     },
-    "masterthestorm": {
-        "link": "http://www.facebook.com/MasterTheStorm",
-        "name": "Master the Storm 2013",
-        "id": "1",
-        "pledgeText": "I pledge, to the best of my ability, to save power for the duration of the 2013 Master the Storm competition, with the help of these guidelines:",
+
+    "ew2014week2": {
+        "link": "http://www.facebook.com/StanfordEnergyWars",
+        "name": "Week 2: Vampire slayers unite!",
+        "active": true,
+        "id": "6",
+        "pledgeText": "This week...",
         "pledgeItems": [
-            "Turn off the lights when I leave a room.",
-            "Study in the sunlight when I can.",
-            "Combine loads in the clothes dryers.",
-            "Set my computer to sleep or go dark automatically.",
-            "Unplug chargers and devices when I'm not using them."
+            "I will set my computer to sleep or hibernate automatically.",
+            "I will unplug my chargers and other idle electronics when not in use.",
+            "I will turn off my power strip when not in use.",
+            "I will set my refrigerator to a lighter temperature setting."
         ],
         "teamList": [
             {
-                "name": "Stern",
-                "size": "573",
+                "name": "Burbank",
+                "size": "94",
                 "id": "0"
             },
             {
-                "name": "Wilbur",
-                "size": "708",
+                "name": "Junipero",
+                "size": "93",
                 "id": "1"
+            },
+            {
+                "name": "Larkin",
+                "size": "104",
+                "id": "2"
+            },
+            {
+                "name": "Twain",
+                "size": "106",
+                "id": "3"
+            },
+            {
+                "name": "Donner",
+                "size": "94",
+                "id": "4"
+            },
+            {
+                "name": "Serra",
+                "size": "92",
+                "id": "5"
+            },
+            {
+                "name": "Cedro",
+                "size": "93",
+                "id": "6"
+            },
+            {
+                "name": "Casa Zapata",
+                "size": "79",
+                "id": "7"
+            },
+            {
+                "name": "Rinconada",
+                "size": "93",
+                "id": "8"
+            },
+            {
+                "name": "Trancos",
+                "size": "82",
+                "id": "9"
+            },
+            {
+                "name": "Soto",
+                "size": "93",
+                "id": "10"
+            },
+            {
+                "name": "Okada",
+                "size": "76",
+                "id": "11"
+            },
+            {
+                "name": "Arroyo",
+                "size": "84",
+                "id": "12"
+            },
+            {
+                "name": "Otero",
+                "size": "91",
+                "id": "13"
             }
         ],
-        "auxMessage": "The team that has the greatest percentage of pledges on April 26th will win up to $500 in prizes! You can also create a link below to send to your friends in the competition and rack up referrals, possibly entailing cool prizes. :)"
+        "auxMessage": "Even when your charger is not charging your phone, it still sucks energy, aka Vampire Energy, if plugged to the wall. Help slay these nasty suckers by unplugging your chargers when you’re not using them. THIS WEEK'S RAFFLE IS OVER. The winners of the five $20 Coupa gift cards are: Dylan Sarkisian from Soto, Alice Bertherat from Cedro, Matthew Stevens from J-Ro, and Griffin Hill and Jacobo Ochoa from Otero.",
+        "logoSrc": "img/energywars2014.png"
+    },
+
+    "ew2014week3": {
+        "link": "http://www.facebook.com/StanfordEnergyWars",
+        "name": "Week 3: The Clean Green",
+        "active": true,
+        "id": "7",
+        "pledgeText": "This week...",
+        "pledgeItems": [
+            "I will only dry full loads of laundry.",
+            "I will wash my clothes on the \“cold\” setting of the washing machine.",
+            "I will hang my clothes up to dry if weather permits.",
+            "I will clean the dryer filter after each use.",
+            "I will not overdry my clothes."
+        ],
+        "teamList": [
+            {
+                "name": "Burbank",
+                "size": "94",
+                "id": "0"
+            },
+            {
+                "name": "Junipero",
+                "size": "93",
+                "id": "1"
+            },
+            {
+                "name": "Larkin",
+                "size": "104",
+                "id": "2"
+            },
+            {
+                "name": "Twain",
+                "size": "106",
+                "id": "3"
+            },
+            {
+                "name": "Donner",
+                "size": "94",
+                "id": "4"
+            },
+            {
+                "name": "Serra",
+                "size": "92",
+                "id": "5"
+            },
+            {
+                "name": "Cedro",
+                "size": "93",
+                "id": "6"
+            },
+            {
+                "name": "Casa Zapata",
+                "size": "79",
+                "id": "7"
+            },
+            {
+                "name": "Rinconada",
+                "size": "93",
+                "id": "8"
+            },
+            {
+                "name": "Trancos",
+                "size": "82",
+                "id": "9"
+            },
+            {
+                "name": "Soto",
+                "size": "93",
+                "id": "10"
+            },
+            {
+                "name": "Okada",
+                "size": "76",
+                "id": "11"
+            },
+            {
+                "name": "Arroyo",
+                "size": "84",
+                "id": "12"
+            },
+            {
+                "name": "Otero",
+                "size": "91",
+                "id": "13"
+            }
+        ],
+        "auxMessage": "There's no need to bake your clothes! Take care of your clothing and the environment with smarter laundry habits. THIS WEEK'S RAFFLE IS OVER. The winner of the five $20 Amazon gift certificates are: Alan Khaledi and Sarah Chung from Otero, Carla Schneider from Donner, Linda Xiong from J-Ro, and Paul Lai from Trancos.",
+        "logoSrc": "img/energywars2014.png"
     }
 };
 
